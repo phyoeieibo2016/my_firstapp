fe1d72f906405b8f4e067e383424f6756ba0bf05
phyoeiei.bo <phyoeieibo.peeb@gmail.com>
2020-03-23 08:53:05 +0630

    from first activity to second activity

---
+++ app/src/main/java/com/example/GetmeanappActivity.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GetmeanappActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_getmeanapp)
+    }
+}
